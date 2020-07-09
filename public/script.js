dac2114f95bba20f73ff33121d47dc41669da95c
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-07-09 14:52:47 +0100

    fix: filter method in search pokedex

--- public/script.js
+++ public/script.js
@@ -1,23 +1,30 @@
 console.log("test");
 
 document.onreadystatechange = function () {
-    if (document.readyState === "complete") {
-        fetch("../data/pokedex.txt")
-            .then(response => {
-                return response.text()})
-            .then((data) => {;
-                searchPokedex(data)
-                //raw text file is logged here
-            })
-    }
+  if (document.readyState === "complete") {
+    fetch("../data/pokedex.txt")
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        searchPokedex(data);
+        //raw text file is logged here
+      });
+  }
+};
+
+function searchPokedex(input, query = "A") {
+  //Uses string methods to search pokedex.txt to filter the list of pokemon
+  const splitInput = input.split("\n");
+  const Output = splitInput.filter((word) => {
+    //words first letter is not query
+    return word.startsWith(query);
+  });
+  console.log(Output.join("\n"));
 }
 
-function searchPokedex (input, query = "A") {
-    //Uses string methods to search pokedex.txt to filter the list of pokemon
-    const splitInput = input.split("\n");
-    const Output = splitInput.filter((word) => {
-        //words first letter is not query
-        return !word.startsWith(query)
-    })
-    console.log(Output.join("\n"))
-}
+// const input = document.getElementById('input')
+
+// input.addEventListener('keyup', function(event) {
+//     searchPokedex(event.target.in)
+// })
