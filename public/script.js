5488c91e84c8df1799180bed819dee9d4b88861c
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-07-10 18:40:20 +0100

    fix: accessibility

--- public/script.js
+++ public/script.js
@@ -1,24 +1,22 @@
-
 function pokedexFetch() {
-    fetch("../data/pokedex.txt")
-        .then((response) => {
-            return response.text();
-        })
-        .then((data) => {
-            searchPokedex(data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        })
+  fetch("../data/pokedex.txt")
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      searchPokedex(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 function searchPokedex(input) {
-    //logs filtered list for a specific query
-    const query = document.getElementById('input').value;
-    const splitInput = input.split("\n");
-    const output = splitInput.filter((word) => {
-        return word.startsWith(query);
-    });
-    console.log(output.join("\n"));
+  //logs filtered list for a specific query
+  const query = document.getElementById("input").value;
+  const splitInput = input.split("\n");
+  const output = splitInput.filter((word) => {
+    return word.startsWith(query);
+  });
+  console.log(output.join("\n"));
 }
-
