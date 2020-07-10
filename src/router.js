7e2e8bb982cc8d9c2fbc53290fe2f9c58dcc16cb
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-07-10 13:23:16 +0100

    reach pokedex.txt endpoint

--- src/router.js
+++ src/router.js
@@ -1,11 +1,20 @@
-var http = require('http');
-var fs = require('fs');
+var http = require("http");
+var fs = require("fs");
 
 function router(request, response) {
   const endpoint = request.url;
   if (endpoint === "/") {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile(__dirname + '/../public/index.html', function(error, file) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile(__dirname + "/../public/index.html", function (error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  } else if (endpoint === "/data/pokedex.txt") {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    fs.readFile(__dirname + `/../data/pokedex.txt`, function (error, file) {
       if (error) {
         console.log(error);
         return;
@@ -14,24 +23,24 @@ function router(request, response) {
     });
   } else {
     console.log(endpoint);
-    const extension = endpoint.split(".")[1]
+    const extension = endpoint.split(".")[1];
     const headers = {
-        "html": "text/html",
-        "css": "text/css",
-        "js":"application/javascript",
-        "ico": "image/x-icon",
-        "jpg": "image/jpeg",
-        "png": "image/png",
-        "txt": "text/plain"
-    }
-    response.writeHead(200, {"Content-Type": headers[extension]})
-    fs.readFile(__dirname + `/../public${endpoint}`, function(error, file) {
-        if (error) {
-            console.log(error);
-            return;
-        }
-        response.end(file);
-    })
+      html: "text/html",
+      css: "text/css",
+      js: "application/javascript",
+      ico: "image/x-icon",
+      jpg: "image/jpeg",
+      png: "image/png",
+      txt: "text/plain",
+    };
+    response.writeHead(200, { "Content-Type": headers[extension] });
+    fs.readFile(__dirname + `/../public${endpoint}`, function (error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
   }
 }
 
