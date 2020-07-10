d12fcf14fc4e12bcaa5c65bf2370784874b1de10
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-07-10 13:33:58 +0100

    feat: display results in DOM

--- public/script.js
+++ public/script.js
@@ -1,24 +1,27 @@
-
 function pokedexFetch() {
-    fetch("../data/pokedex.txt")
-        .then((response) => {
-            return response.text();
-        })
-        .then((data) => {
-            searchPokedex(data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        })
+  fetch("../data/pokedex.txt")
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      searchPokedex(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 function searchPokedex(input) {
-    //logs filtered list for a specific query
-    const query = document.getElementById('input').value;
-    const splitInput = input.split("\n");
-    const output = splitInput.filter((word) => {
-        return word.startsWith(query);
-    });
-    console.log(output.join("\n"));
+  //logs filtered list for a specific query
+  const query = document.getElementById("input").value;
+  const splitInput = input.split("\n");
+  const output = splitInput.filter((word) => {
+    return word.startsWith(query);
+  });
+  showResults(output.join("\n"));
 }
 
+function showResults(searchOutput) {
+  const results = document.getElementById("output");
+  results.innerHTML = searchOutput;
+}
